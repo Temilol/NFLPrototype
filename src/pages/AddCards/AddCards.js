6e398b9dd1dca77562f6a3983d76da876c75b5bc
Temilola Aderibigbe <temz4rill@gmail.com>
2019-03-08 16:17:28 -0600

    Added more styles for the Scrollable cards

--- src/pages/AddCards/AddCards.js
+++ src/pages/AddCards/AddCards.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import {Container, Col} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 import playersData from '../../assets/data/playerCards'
 import Cards from '../../components/Cardd/Cards'
 import './AddCards.css'
 
-const playerComponent = playersData.map(playerData => <Cards key={playerData} data={playerData} />)
-const personalComponent = playersData.map(playerData => <Cards key={playerData} data={playerData} />)
 
+
+const playerComponent = playersData.slice(0,10).map(playerData => <Cards key={playerData.id} data={playerData} />)
+const personalComponent = playersData.map(playerData => <Cards key={playerData.id} data={playerData} />)
+// const personalComponent = []
 class AddCards extends React.Component {
   constructor(props){
     super(props);
     this.sectionTitle = props.sectionTitle;
   }
 
-    render(props) {
+    render() {
       return (
         <Container>
-          <h2>Starting Lineup</h2>
+          <hr/>
+            <h2>Starting Lineup</h2>
           <hr/>
           {playerComponent}
           <hr/>
-          <h2>My Cards</h2>
+            <h2>My Cards</h2>
           <hr/>
           <div className="scrolling-wrapper">
             <div className="cardStyles">
