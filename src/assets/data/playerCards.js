e6c52493399d82ef2ae9dd172bc35245f3f6f174
Keishon Smith <keishon104@gmail.com>
2019-03-08 11:25:12 -0600

    Pushes Test cards and Mock JSON Data

---
+++ src/assets/data/playerCards.js
@@ -0,0 +1,17 @@
+const playerCards = [
+  {id: 1, first_name: "Cam", last_name: "Newton", rating: 87, jersey_number: 1, tier: "Silver", team: "Carolina Panthers", position:"QB", card_image: '../playerCards/newton_cam.png'  },
+  {id: 2, first_name: "Marshawn", last_name: "Lynch", rating: 95, jersey_number: 24, tier: "Silver", team: "Oakland Raiders", position:"HB", card_image: '../playerCards/lynch_marshawn.png'  },
+  {id: 3, first_name: "Luke", last_name: "Stocker", rating: 0, jersey_number: 88, tier: "Silver", team: "Tennessee Titans", position:"TE", card_image: ''  },
+  {id: 4, first_name: "Deandre", last_name: "Hopkins", rating: 0, jersey_number: 10, tier: "Silver", team: "Tennessee Titans", position:"FS", card_image: ''  },
+  {id: 5, first_name: "Adrian", last_name: "Clayborn", rating: 0, jersey_number: 24, tier: "Silver", team: "Oakland Raiders", position:"WR", card_image: ''  },
+  {id: 6, first_name: "Malcolm", last_name: "Butler", rating: 0, jersey_number: 24, tier: "Silver", team: "Oakland Raiders", position:"WR", card_image: ''  },
+  {id: 7, first_name: "Travaris", last_name: "Cadet", rating: 0, jersey_number: 39, tier: "Silver", team: "Oakland Raiders", position:"WR", card_image: ''  },
+  {id: 8, first_name: "Elandom", last_name: "Roberts", rating: 0, jersey_number: 52, tier: "Silver", team: "Oakland Raiders", position:"WR", card_image: ''  },
+  {id: 9, first_name: "Calvin", last_name: "Ridley", rating: 0, jersey_number: 18, tier: "Silver", team: "Oakland Raiders", position:"WR", card_image: ''  },
+  {id: 10, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: ''  },
+  {id: 11, first_name: "", last_name: "", rating: 0, jersey_number: 24, tier: "Silver", team: "Oakland Raiders", position:"WR", card_image: ''  },
+
+
+];
+
+export default playerCards;
