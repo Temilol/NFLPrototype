bd0905168dc84226da6e914b72374d93ff1302bb
Garrett Tolbert <garrett1.tolbert@gmail.com>
2019-03-09 01:12:53 -0600

    Editing Lineup
    80% complete

--- src/assets/data/playerCards.js
+++ src/assets/data/playerCards.js
@@ -1,18 +1,18 @@
 const playerCards = [
-  {id: 1, first_name: "Cam", last_name: "Newton", rating: 87, jersey_number: 1, tier: "Silver", team: "Carolina Panthers", position:"QB", card_image: './playerCards/newton_cam.png'  },
-  {id: 2, first_name: "Marshawn", last_name: "Lynch", rating: 95, jersey_number: 24, tier: "Silver", team: "Oakland Raiders", position:"HB", card_image: './playerCards/lynch_marshawn.png'  },
-  {id: 3, first_name: "Luke", last_name: "Stocker", rating: 0, jersey_number: 88, tier: "Silver", team: "Tennessee Titans", position:"TE", card_image: './playerCards/stocker_luke.png'  },
-  {id: 4, first_name: "Deandre", last_name: "Hopkins", rating: 0, jersey_number: 10, tier: "Silver", team: "Tennessee Titans", position:"FS", card_image: './playerCards/hopkins_deandre.png'  },
-  {id: 5, first_name: "Malcolm", last_name: "Butler", rating: 0, jersey_number: 24, tier: "Silver", team: "Oakland Raiders", position:"WR", card_image: './playerCards/butler_malcolm.png'  },
-  {id: 6, first_name: "Elandom", last_name: "Roberts", rating: 0, jersey_number: 52, tier: "Silver", team: "Oakland Raiders", position:"WR", card_image: './playerCards/roberts_elandon.png'  },
-  {id: 7, first_name: "Calvin", last_name: "Ridley", rating: 0, jersey_number: 18, tier: "Silver", team: "Oakland Raiders", position:"WR", card_image: './playerCards/ridley_calvin.png'  },
-  {id: 8, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png'  },
-  {id: 9, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png'  },
-  {id: 10, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png'  },
-  {id: 11, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png'  },
-  {id: 12, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png'  },
-  {id: 13, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png'  },
-  {id: 14, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png'  }
+  {id: 1, first_name: "Cam", last_name: "Newton", rating: 87, jersey_number: 1, tier: "Silver", team: "Carolina Panthers", position:"QB", card_image: './playerCards/newton_cam.png', inLineup:false  },
+  {id: 2, first_name: "Marshawn", last_name: "Lynch", rating: 95, jersey_number: 24, tier: "Silver", team: "Oakland Raiders", position:"HB", card_image: './playerCards/lynch_marshawn.png', inLineup:false  },
+  {id: 3, first_name: "Luke", last_name: "Stocker", rating: 0, jersey_number: 88, tier: "Silver", team: "Tennessee Titans", position:"TE", card_image: './playerCards/stocker_luke.png', inLineup:false  },
+  {id: 4, first_name: "Deandre", last_name: "Hopkins", rating: 0, jersey_number: 10, tier: "Silver", team: "Tennessee Titans", position:"FS", card_image: './playerCards/hopkins_deandre.png', inLineup:false  },
+  {id: 5, first_name: "Malcolm", last_name: "Butler", rating: 0, jersey_number: 24, tier: "Silver", team: "Oakland Raiders", position:"WR", card_image: './playerCards/butler_malcolm.png', inLineup:false  },
+  {id: 6, first_name: "Elandom", last_name: "Roberts", rating: 0, jersey_number: 52, tier: "Silver", team: "Oakland Raiders", position:"WR", card_image: './playerCards/roberts_elandon.png', inLineup:false  },
+  {id: 7, first_name: "Calvin", last_name: "Ridley", rating: 0, jersey_number: 18, tier: "Silver", team: "Oakland Raiders", position:"WR", card_image: './playerCards/ridley_calvin.png', inLineup:false  },
+  {id: 8, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png', inLineup:false  },
+  {id: 9, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png', inLineup:false  },
+  {id: 10, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png', inLineup:false  },
+  {id: 11, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png', inLineup:false  },
+  {id: 12, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png', inLineup:false  },
+  {id: 13, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png', inLineup:false  },
+  {id: 14, first_name: "Fabian", last_name: "Moreau", rating: 0, jersey_number: 31, tier: "Silver", team: "Washington Redskins", position:"CB", card_image: './playerCards/moreau_fabian.png', inLineup:false  }
 
 
 ];
--- src/components/Cardd/Cards.js
+++ src/components/Cardd/Cards.js
@@ -1,20 +1,96 @@
 import React from 'react'
 import './Cards.css'
+import $ from 'jquery';
+// const waitingForSwap = false;
+
+function getCard() {
+  document.addEventListener('click',function(event) {
+    console.log("hii: " + event.target.id);
+  }, false);
+}
 
 class Cards extends React.Component {
   constructor() {
     super()
     this.state = {
       num: 0,
+      isLineup: false,
+      waitingForSwap: false,
       placement:["collection","lineup"]
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleSwap = this.handleSwap.bind(this);
+    this.handleHold = this.handleHold.bind(this);
+    this.handleButtonRelease = this.handleButtonRelease.bind(this);
+    this.handleButtonPress = this.handleButtonPress.bind(this);
+  }
+
+  handleSwap(lineupID,swaps,waitingForSwap) {
+    alert(waitingForSwap + "  yoooo");
+    function getCard(event) {
+      var currID = event.target.id;
+      if(currID <= 11 && waitingForSwap === true) {
+        alert("Player already in starting lineup");
+        document.getElementById(currID).style.border = '0';
+        document.getElementById(currID).style.height = '75%';
+        document.getElementById(currID).style.width = '20%';
+        this.handleIncorrectSwap(lineupID,swaps,waitingForSwap);
+      } else if(waitingForSwap === true){
+          document.getElementById(currID).style.border = '4px solid blue';
+          document.getElementById(currID).style.height = '77%';
+          document.getElementById(currID).style.width = '22%';
+          // document.getElementById('awaitingSwap').id = "here";
+      }
+      console.log(currID);
+      swaps.push(lineupID);
+      swaps.push(currID);
+      console.log("SWAPS: " + swaps);
+      document.removeEventListener('mousedown',getCard, waitingForSwap);
+    }
+    document.addEventListener('mousedown',getCard, waitingForSwap);
+
+  }
+
+  handleButtonRelease() {
+    clearTimeout(this.buttonPressTimer);
+  }
+  handleButtonPress() {
+    this.buttonPressTimer = setTimeout(()=> this.handleHold(),2000);
+  }
+
+  handleClick() {
+    if (this.state.waitingForSwap === false && !document.getElementById('awaitingSwap')) {
+      alert("Opening AR");
+    } else {
+      alert("keef-->" + this.state.waitingForSwap);
+        //this.setState({waitingForSwap:false});
+    }
+  }
+
+  handleHold() {
+    const swaps = [];
+    if (this.props.id <= 11) {
+      this.setState({isLineup:true});
+      this.setState({waitingForSwap:true});
+      document.getElementById('here').id = "awaitingSwap";
+      this.handleSwap(this.props.id,swaps,this.state.waitingForSwap);
+      // alert("proper choice. choose next card");
+
+      document.getElementById(this.props.id).style.border = '4px solid red';
+      document.getElementById(this.props.id).style.height = '77%';
+      document.getElementById(this.props.id).style.width = '22%';
     }
   }
 
   render() {
+
+
     return (
-      <img className="cardStyle" id={this.state.num} src={this.props.data.card_image} alt="player" />
-    )
+        <img className="cardStyle" id={this.props.id} onMouseLeave={this.handleButtonRelease} onMouseUp={this.handleButtonRelease} onMouseDown={this.handleButtonPress} onClick={this.handleClick} src={this.props.data.card_image} alt="player" />
+
+  );
   }
 }
+
 
 export default Cards
