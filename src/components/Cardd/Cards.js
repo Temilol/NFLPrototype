58a3fa6493b618b78b7733b1473739a87edbf186
Temilola Aderibigbe <temz4rill@gmail.com>
2019-03-08 18:26:52 -0600

    Working on dynamic image id

--- src/components/Cardd/Cards.js
+++ src/components/Cardd/Cards.js
@@ -2,9 +2,17 @@ import React from 'react'
 import './Cards.css'
 
 class Cards extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      num: 0,
+      placement:["collection","lineup"]
+    }
+  }
+
   render() {
     return (
-      <img className="cardStyle" src={this.props.data.card_image} alt="player" />
+      <img className="cardStyle" id={this.state.num} src={this.props.data.card_image} alt="player" />
     )
   }
 }
--- src/pages/AddCards/AddCards.js
+++ src/pages/AddCards/AddCards.js
@@ -8,6 +8,17 @@ import './AddCards.css'
 
 const playerComponent = playersData.slice(0,10).map(playerData => <Cards key={playerData.id} data={playerData} />)
 const personalComponent = playersData.map(playerData => <Cards key={playerData.id} data={playerData} />)
+
+// const test = document.getElementsByClassName('App');
+// // aler
+// const imgTags = document.getElementsByTagName('img');
+// var i = 0;
+// console.log(imgTags.length);
+// for(;i < imgTags.length; i++){
+//   alert("Here!!!");
+//   console.log(this.state.num += 1);
+// }
+
 // const personalComponent = []
 class AddCards extends React.Component {
   constructor(props){
