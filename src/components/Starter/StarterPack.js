2a645fc5059c3832fa4a2b9309b59b9b9116abfa
Temilola Aderibigbe <temz4rill@gmail.com>
2019-03-09 04:08:41 -0600

    Add a shuffled market to the Trade screen and Changed the starter pack icon

--- src/components/Starter/StarterPack.js
+++ src/components/Starter/StarterPack.js
@@ -38,7 +38,7 @@ class StarterPack extends React.Component{
              <Modal.Title className="nameStyle">Starter Pack</Modal.Title>
            </Modal.Header>
            <Modal.Body>
-              <img className="imageStyle" src={"./playerCards/lynch_marshawn.png"} alt="Starter Icon" />
+              <img className="imageStyle" src={"./playerCards/starter_pack.png"} alt="Starter Icon" />
            </Modal.Body>
            <Modal.Footer  className="buttonStyle">
              <Button size="lg" variant="secondary" href={"/cards"}>
